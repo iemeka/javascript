7074c3167e66bf7d7b48d02589eabad83ccb364f
iemeka <mekstines@gmail.com>
2019-03-02 10:00:15 +0100

    exercise one

---
+++ eloquent.js
@@ -0,0 +1,51 @@
+//- exercise one
+let ash = ""
+for(let i=0;i<7;i++){
+    ash +="#";
+    console.log(ash);
+}
+
+let grid = 5 ,boxTwo = "", box = "";
+
+for(j=1;j < grid; j++){
+    if (j == 1) box += " ";
+
+    if ( j % 2 == 0){
+        box += " "
+    }else{
+        box += "#"
+    }
+}
+
+for(j=1;j < grid; j++){
+    if (j == 1) boxTwo += "#";
+
+    if ( j % 2 == 0){
+        boxTwo += "#"
+    }else{
+        boxTwo +=" "
+    }
+}
+
+for (j=0;j<grid;j++){
+    if (j%2==0){
+        console.log(box)
+    }else{
+        console.log(boxTwo)
+    }
+}
+
+
+
+for(let j=1; j<=100;j++){
+    if(j%5==0 && j%3==0){
+        console.log("FizzBuzz")
+    }else if(j%3==0){
+        console.log("Fizz")
+    }else if(j%5==0){
+        console.log("Buzz")
+    }else{
+        console.log(j)
+    }
+    
+}
